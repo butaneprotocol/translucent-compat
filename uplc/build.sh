9fbdcfe0766afc939c4e3be9b5cb7434dafc241c
Micah Kendall <micahk.accounts@icloud.com>
2023-12-22 23:36:57 +1100

    removed uplc

--- uplc/build.sh
+++
@@ -1,7 +0,0 @@
-rm -rf pkg
-rm -rf pkg-node
-rm -rf pkg-web
-wasm-pack build -t nodejs
-mv pkg pkg-node
-wasm-pack build
-mv pkg pkg-web
--- uplc/pkg-web/uplc_wrapper.js
+++
@@ -1,4 +0,0 @@
-import * as wasm from "./uplc_wrapper_bg.wasm";
-import { __wbg_set_wasm } from "./uplc_wrapper_bg.js";
-__wbg_set_wasm(wasm);
-export * from "./uplc_wrapper_bg.js";
