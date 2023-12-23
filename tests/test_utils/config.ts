5f3e4293f7435ee3974e92b8f76aa58eae74624d
Joaquin Hoyos <joaco.hoyos@hotmail.com>
2023-12-23 17:27:14 -0300

    add support for ogmios 5 and 6

--- tests/test_utils/config.ts
+++ tests/test_utils/config.ts
@@ -1,6 +1,13 @@
 const CONFIG = {
-  OGMIOS_URL: process.env.OGMIOS_URL,
-  KUPO_URL: process.env.KUPO_URL,
-  SEED_PHRASE: process.env.SEED_PHRASE,
+  KUPO_PREVIEW_URL: process.env.KUPO_PREVIEW_URL ?? "",
+  KUPO_PREVIEW_APIKEY: process.env.KUPO_PREVIEW_APIKEY ?? "",
+
+  OGMIOS_PREVIEW_URL: process.env.OGMIOS_PREVIEW_URL ?? "",
+  OGMIOS_PREVIEW_APIKEY: process.env.OGMIOS_PREVIEW_APIKEY ?? "",
+  USE_OGMIOS_V5: process.env.USE_OGMIOS_V5 === "1",
+
+  MAESTRO_API_KEY: process.env.MAESTRO_API_KEY ?? "",
+
+  SEED_PHRASE: process.env.SEED_PHRASE ?? "",
 };
 export default CONFIG;
