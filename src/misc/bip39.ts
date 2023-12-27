4223918f4fc7fecccc29ec6cf2732c890fc0ace4
Pal Dorogi <pal.dorogi@gmail.com>
2023-12-27 11:46:44 +0100

    Added blockfrost, blueprint, fixed bip39

--- src/misc/bip39.ts
+++ src/misc/bip39.ts
@@ -131,7 +131,7 @@ function entropyToMnemonic(
 function deriveChecksumBits(entropyBuffer: Uint8Array): string {
   const ENT = entropyBuffer.length * 8;
   const CS = ENT / 32;
-  const hash = sha256(entropyBuffer.toString(), { asBytes: true });
+  const hash = sha256(Array.from(entropyBuffer), { asBytes: true });
   return bytesToBinary(Array.from(hash)).slice(0, CS);
 }
 
