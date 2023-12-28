5f91ddb1475c05495804e55422eac90a63539e2d
Micah <66159982+micahkendall@users.noreply.github.com>
2023-12-29 04:12:00 +1100

    Chained Wallet (#23)
    * chained wallet
    
    * expose wallets
    
    * fixed <= bug

--- src/mod.ts
+++ src/mod.ts
@@ -3,4 +3,5 @@ export * from "./translucent/mod.ts";
 export * from "./provider/mod.ts";
 export * from "./types/mod.ts";
 export * from "./utils/mod.ts";
+export * from "./wallets/mod.ts";
 export * from "./plutus/mod.ts";
